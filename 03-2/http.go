bab3c88f54ffcd0234e008ef04cf04eb9fa33b29
yeqianmen <56306966+yeqianmen@users.noreply.github.com>
2023-12-10 01:29:15 +0800

    http

---
+++ 03-2/http.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_log"])
+	for k, v := range r.Form {
+		fmt.Println("key: ", k)
+		fmt.Println("value: ", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "hello astaxie!")
+
+}
+func main() {
+	http.HandleFunc("/", sayhelloName)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
